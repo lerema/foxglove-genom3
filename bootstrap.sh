ef6fe8edeef14b6714ac3d79b4a6422e927ed8bd
franklinselva <franklinselva10@gmail.com>
2023-08-30 15:56:49 +0200

    adapt file generation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+sudo apt install -y libwebsocketpp-dev nlohmann-json3-dev flatbuffers-compiler libflatbuffers-dev
+
+flatc --schema -b -o flatbuffers/schema flatbuffers/definitions/*.fbs
 autoreconf -vi
+
+# Compile flatbuffers
+flatc --cpp -o codels/foxglove/generated flatbuffers/definitions/*.fbs
